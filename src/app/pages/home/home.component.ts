5f2505ed4487da3fb1e75c883d3485541988dce7
Vothy <dev.vothyprak@gmail.com>
2026-06-04 10:44:53 -0400

    update meta and icon

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal, OnInit, OnDestroy } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Component, signal, OnInit, OnDestroy, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -16,7 +17,6 @@ export class HomeComponent implements OnInit, OnDestroy {
   ];
   currentRoleIndex = signal(0);
 
-  // 1. Add a signal to track the animation state flag
   isAnimating = signal(true);
   private intervalId: any;
 
@@ -24,16 +24,18 @@ export class HomeComponent implements OnInit, OnDestroy {
   name = signal('Vothy Prak');
   location = signal('North Carolina');
 
+  private titleService = inject(Title);
+
+  constructor() {
+    this.titleService.setTitle('Home | Vothy Prak | Technical Analyst');
+  }
+
   ngOnInit() {
     this.intervalId = setInterval(() => {
-      // 2. Turn animation off
       this.isAnimating.set(false);
 
-      // 3. Move to the next string role target
       this.currentRoleIndex.update(index => (index + 1) % this.roles.length);
 
-      // 4. A tiny 20ms delay lets the browser register the class removal 
-      // before we snap it back on to trigger the keyframes again!
       setTimeout(() => {
         this.isAnimating.set(true);
       }, 20);
